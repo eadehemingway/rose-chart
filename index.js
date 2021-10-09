172aff332eca2109a9db6e2d76bb582085991b88
Eade Hemingway <eadehemingway@Eades-MBP.home>
2021-10-09 23:03:40 +0200

    make chart have three color wedges

--- index.js
+++ index.js
@@ -1,51 +1,90 @@
-var arcData = [
-  { amount: 7 },
-  { amount: 8 },
-  { amount: 9 },
-  { amount: 10 },
-  { amount: 11 },
-  { amount: 12 },
-  { amount: 13 },
-  { amount: 14 },
-  { amount: 15 },
+const arc_data = [
+  { label: "label", black_bit: 1, red_bit: 1, blue_bit: 2 },
+  { label: "label", black_bit: 2, red_bit: 6, blue_bit: 28 },
+  { label: "label", black_bit: 2, red_bit: 7, blue_bit: 11 },
+  { label: "label", black_bit: 2, red_bit: 7.7, blue_bit: 23 },
+  { label: "label", black_bit: 1, red_bit: 6, blue_bit: 12 },
+  { label: "label", black_bit: 1, red_bit: 8, blue_bit: 28 },
+  { label: "label", black_bit: 2, red_bit: 7, blue_bit: 24 },
+  { label: "label", black_bit: 1, red_bit: 6, blue_bit: 13 },
+  { label: "label", black_bit: 3, red_bit: 5, blue_bit: 8 },
+  { label: "label", black_bit: 3, red_bit: 5, blue_bit: 19 },
+  { label: "label", black_bit: 3, red_bit: 5, blue_bit: 10 },
+  { label: "label", black_bit: 3, red_bit: 5, blue_bit: 3 },
 ];
 
-var full_circle_in_radians = 6.283185307179586;
-var angle_of_wedge = full_circle_in_radians / arcData.length;
+const full_circle_in_radians = 6.283185307179586;
+const angle_of_wedge = full_circle_in_radians / arc_data.length;
 
-var all_amounts = arcData.map((d) => d.amount);
-var radiusScale = d3.scaleLinear().domain(all_amounts).range([50, 70]);
-var arc_data_with_start_and_end = arcData.map((val, i) => {
-  var startAngle = i * angle_of_wedge;
-  var endAngle = startAngle + angle_of_wedge;
+const all_values = [];
+arc_data.forEach((d) => {
+  all_values.push(d.black_bit, d.red_bit, d.blue_bit);
+});
+
+const domain = d3.extent(all_values);
+
+const radiusScale = d3.scaleLinear().domain(domain).range([10, 200]);
+const arc_data_with_start_and_end = arc_data.map((val, i) => {
+  const startAngle = i * angle_of_wedge;
+  const endAngle = startAngle + angle_of_wedge;
   return {
-    radius: radiusScale(val.amount),
+    ...val,
     startAngle,
     endAngle,
   };
 });
 
-var angleScale = d3
+const angleScale = d3
   .scaleLinear()
   .range([Math.PI, 3 * Math.PI])
-  .domain([0, arcData.length]);
+  .domain([0, arc_data.length]);
 
-var svg = d3.select("svg").append("g").attr("transform", "translate(300,300)");
+const svg = d3
+  .select("svg")
+  .append("g")
+  .attr("transform", "translate(300,300)")
+  .attr("class", "whole-rose-chart");
 
 // Create an arc generator with configuration
-var arcGenerator = d3
-  .arc()
-  .innerRadius(0)
-  .outerRadius(function (d, i) {
-    return d.radius;
-  }); // needs to be dynamic
+const get_arc_generator = (color) =>
+  d3
+    .arc()
+    .innerRadius(0)
+    .outerRadius(function (d, i) {
+      return radiusScale(d[color]);
+    });
 
-// Create a path element and set its d attribute
-d3.select("g")
+const whole_chart_group = d3.select(".whole-rose-chart");
+
+whole_chart_group
+  .append("g")
+  .attr("class", "blue-bits")
   .selectAll("path")
   .data(arc_data_with_start_and_end)
   .join("path")
-  .attr("d", arcGenerator)
-  .attr("fill", "orange")
+  .attr("d", (d) => get_arc_generator("blue_bit")(d))
+  .attr("fill", "rgb(198, 226, 255)")
   .attr("stroke-width", 1)
-  .attr("stroke", "black");
+  .attr("stroke", "grey");
+
+whole_chart_group
+  .append("g")
+  .attr("class", "red-bits")
+  .selectAll("path")
+  .data(arc_data_with_start_and_end)
+  .join("path")
+  .attr("d", (d) => get_arc_generator("red_bit")(d))
+  .attr("fill", "rgb(252,112,147)")
+  .attr("stroke-width", 1)
+  .attr("stroke", "grey");
+
+whole_chart_group
+  .append("g")
+  .attr("class", "black-bits")
+  .selectAll("path")
+  .data(arc_data_with_start_and_end)
+  .join("path")
+  .attr("d", (d) => get_arc_generator("black_bit")(d))
+  .attr("fill", "rgb(50,50,50)")
+  .attr("stroke-width", 1)
+  .attr("stroke", "grey");
